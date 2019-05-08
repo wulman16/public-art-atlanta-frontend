7595f138983877722296bb2aea09c2db9fd0380c
wulman16 <will.ulman@gmail.com>
2019-05-08 12:26:51 -0400

    render sort modal

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,14 +1,36 @@
 import React, { Component, Fragment } from 'react'
+import ReactModal from 'react-modal'
 import SortDialog from './SortDialog'
 import CardContainer from './CardContainer'
 
 class BodyContainer extends Component {
+
+  state = {
+    showSort: false
+  }
+
+  toggleSort = () => {
+    this.setState({
+      showSort: !this.state.showSort
+    })
+  }
+
   render() {
     return(
       <Fragment>
-        <SortDialog handleSort={this.props.handleSort}
-                    handleFilter={this.props.handleFilter}
-                    handleSeenFilter={this.props.handleSeenFilter} />
+        <div>
+          <ReactModal
+            isOpen={this.state.showSort}
+            contentLabel="Sort and Filter Options">
+            <SortDialog
+              handleSort={this.props.handleSort}
+              handleFilter={this.props.handleFilter}
+              handleSeenFilter={this.props.handleSeenFilter}
+              closeDialog={this.toggleSort} />
+            <button onClick={this.toggleSort}>Close Modal</button>
+          </ReactModal>
+          <button onClick={this.toggleSort}>Sort and Filter Artworks</button>
+        </div>
         <CardContainer seen={this.props.seen}
                        artworks={this.props.artworks}
                        openDetails={this.props.openDetails} />
--- src/components/Index.js
+++ src/components/Index.js
@@ -105,23 +105,6 @@ class Index extends Component {
         <div className="site-name-container">
           <h1 className="site-name">Public Art Atlanta</h1>
         </div>
-        <Details show={this.state.detailsOpen}
-                 onClose={this.closeDetails}
-                 profile={this.state.profile}
-                 id={this.state.id}
-                 title={this.state.title}
-                 artist={this.state.artist}
-                 year={this.state.year}
-                 image={this.state.image}
-                 lat={this.state.lat}
-                 lng={this.state.lng}
-                 source={this.state.source}
-                 owner={this.state.owner}
-                 seen={this.props.seen}
-                 handleSeen={this.props.handleSeen} />
-        <NewArtworkForm show={this.state.newOpen}
-                        onClose={this.closeNew}
-                        handleArtworkSubmit={this.props.handleArtworkSubmit} />
         <Header userId={this.props.userId}
                 userName={this.props.userName}
                 seen={this.props.seen}
